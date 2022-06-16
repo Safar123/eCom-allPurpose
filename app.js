bbe393bf83a4cf247988ef99d5e14efcedaabb70
Safar123 <azajsbd111@gmail.com>
2022-06-16 10:07:24 +0545

    implementing error handler and 404 route

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const errorHandler = require('./middleware/errorHandler');
+const GlobalError = require('./utils/globalError');
 
 const app =express();
 
+app.use(express.json());
 
+
+//defining routes
+const productRoute = require('./routes/productRoute');
+app.use('/api/v1', productRoute);
+
+//handling all unhandled error
+app.all('*', (req,res,next)=>{
+    const message = `${req.originalUrl} doesn't exist or not defined yet`;
+    return next (new GlobalError(message, 404))
+})
+app.use(errorHandler);
 module.exports=app;
