ebc961f42a14ef0db2f3faa52ce798440ee537b5
Safar123 <azajsbd111@gmail.com>
2022-06-18 09:44:27 +0545

    resize and upload middleware

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,12 +1,24 @@
-const express = require('express');
-const { allProducts, addNewProduct, updateProduct, removeProduct, letsBuyProduct } = require('../controller/productController');
+const express = require("express");
+const {
+    allProducts,
+    addNewProduct,
+    updateProduct,
+    removeProduct,
+    letsBuyProduct,
+    uploadProductImage,
+    resizeProductsImage,
+} = require("../controller/productController");
 
 const router = express.Router();
 
-router.route('/products').get(allProducts).post(addNewProduct);
-router.route('/products/:id').patch(updateProduct).delete(removeProduct).post(letsBuyProduct);
-
-
-
+router
+    .route("/products")
+    .get(allProducts)
+    .post(uploadProductImage, resizeProductsImage ,addNewProduct);
+router
+    .route("/products/:id")
+    .patch(updateProduct)
+    .delete(removeProduct)
+    .post(letsBuyProduct);
 
 module.exports = router;
