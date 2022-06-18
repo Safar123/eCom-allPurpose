3fddf04210c8b1799a47c724de8fb8660102f540
Safar123 <azajsbd111@gmail.com>
2022-06-18 09:40:37 +0545

    Created username from user email input

--- model/userModel.js
+++ model/userModel.js
@@ -1,56 +1,63 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const bcrypt = require('bcrypt')
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-
-    username:{
-        type:String,
-        
+    username: {
+        type: String,
     },
-    email:{
-        type:String,
-        required:[true, 'Please provide valid email information'],
-        validate: [validator.isEmail, 'Please provide valid email'],
-        trim:true,
-        unique:[true, 'Email already exist please try log in or register with new email'],
-        lowercase:true,
-    },
-
-    password:{
-        type:String,
-        required:[true, 'Password is required and must be 8 character long'],
-        trim:true,
-        minlength:[8, 'Password must be 8 character long']
+    email: {
+        type: String,
+        required: [true, "Please provide valid email information"],
+        validate: [validator.isEmail, "Please provide valid email"],
+        trim: true,
+        unique: [
+            true,
+            "Email already exist please try log in or register with new email",
+        ],
+        lowercase: true,
     },
 
-    confirmPassword:{
-        type:String,
-        validate:{
-  validator:function(el){
-    return el ===this.password
-  }
+    password: {
+        type: String,
+        required: [true, "Password is required and must be 8 character long"],
+        trim: true,
+        minlength: [8, "Password must be 8 character long"],
+        select: false,
+    },
+
+    confirmPassword: {
+        type: String,
+        validate: {
+            validator: function (el) {
+                return el === this.password;
+            },
         },
-        message:'Password field did not match'
+        message: "Password field did not match",
     },
 
-    userImage:{
-        type:String,
+    userImage: {
+        type: String,
     },
 
-    role:{
-        type:String,
-        enum:['user', 'admin', 'super-admin'],
-        default:'user'
-    }
+    role: {
+        type: String,
+        enum: ["user", "admin", "super-admin"],
+        default: "user",
+    },
 });
 
-userSchema.pre('save', async function(next){
-    if(!this.isModified('password')) return next;
-    this.password= await bcrypt.hash(this.password, 12);
-    this.confirmPassword=undefined;
-})
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next;
+    this.password = await bcrypt.hash(this.password, 12);
+    this.username = await `${this.email}`.split("@")[0];
+    this.confirmPassword = undefined;
+});
 
-const User = mongoose.model('User', userSchema);
+userSchema.methods.matchUserPassword = async function (userPswd, password) {
+    return await bcrypt.compare(userPswd, password);
+};
 
-module.exports= User;
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
