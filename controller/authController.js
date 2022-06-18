96b89be4bf7da63ad074d620abd8c4ce03f3640e
Safar123 <azajsbd111@gmail.com>
2022-06-18 09:40:06 +0545

    Login function

--- controller/authController.js
+++ controller/authController.js
@@ -1,25 +1,48 @@
-const User = require('../model/userModel');
-const catchAsync = require('../utils/catchAsyncError');
-const GlobalError = require('../utils/globalError');
+const User = require("../model/userModel");
+const catchAsync = require("../utils/catchAsyncError");
+const GlobalError = require("../utils/globalError");
 
-exports.signUpUser = catchAsync(async (req,res, next)=>{
-  
-    const username=`${req.body.email}`.split('@')[0];
+exports.signUpUser = catchAsync(async (req, res, next) => {
+    if (!req.body.email) {
+        return next(new GlobalError("Email is required field", 400));
+    }
     const newUser = await User.create({
-        username:username,
-        email:req.body.email,
-        password:req.body.password,
-        confrimPassword:req.body.password,
-        userImage:req.body.userImage,
-        
-    })
-    
-    if(!newUser) return next(new GlobalError('Something went wrong creating user', 500));
+        email: req.body.email,
+        password: req.body.password,
+        confrimPassword: req.body.password,
+        userImage: req.body.userImage,
+    });
+
+    if (!newUser)
+        return next(new GlobalError("Something went wrong creating user", 500));
 
     res.status(201).json({
-        success:'true',
-        data:{
-            user:newUser
-        }
-    })
-})
+        success: "true",
+        data: {
+            user: newUser,
+        },
+    });
+});
+
+exports.logInUser = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return next(new GlobalError("Email and password is required field", 400));
+    }
+
+    const emailExist = await User.findOne({ email: email }).select("+password");
+
+    if (
+        !emailExist ||
+        !(await emailExist.matchUserPassword(password, emailExist.password))
+    ) {
+        return next(new GlobalError("Either email or password is incorrect", 400));
+    }
+
+    res.status(200).json({
+        success: "True",
+        data: {
+            user: emailExist,
+        },
+    });
+});
