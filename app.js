94c3e22c700740076cb754bbde5553a7fa0a1aaf
Safar123 <azajsbd111@gmail.com>
2022-06-18 09:39:44 +0545

    prettier indentation

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-const express = require('express');
-const errorHandler = require('./middleware/errorHandler');
-const GlobalError = require('./utils/globalError');
+const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
+const GlobalError = require("./utils/globalError");
 
-const app =express();
+const app = express();
 
 app.use(express.json());
 
-
 //defining routes
-const productRoute = require('./routes/productRoute');
-const userRoute = require('./routes/userRouter');
-app.use('/api/v1', productRoute);
-app.use('/api/v1/user', userRoute);
+const productRoute = require("./routes/productRoute");
+const userRoute = require("./routes/userRouter");
+app.use("/api/v1", productRoute);
+app.use("/api/v1/user", userRoute);
 
 //handling all unhandled error
-app.all('*', (req,res,next)=>{
+app.all("*", (req, res, next) => {
     const message = `${req.originalUrl} doesn't exist or not defined yet`;
-    return next (new GlobalError(message, 404))
-})
+    return next(new GlobalError(message, 404));
+});
 app.use(errorHandler);
-module.exports=app;
+module.exports = app;
